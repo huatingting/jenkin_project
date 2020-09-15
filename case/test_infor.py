e040cd085abcff724c0b59ebab75e86cd68e9e9a
华华 <huatingtingnet@163.com>
2020-09-15 17:32:40 +0800

    zengjia

---
+++ case/test_infor.py
@@ -0,0 +1,5 @@
+class TestInfo:
+    def test_info001(self):
+        assert 1
+    def test_info_002(self):
+        assert 0
--- case/test_login.py
+++ case/test_login.py
@@ -5,3 +5,5 @@ class TestLogin:
         assert 0
     def test_003(self):
         assert 1
+    def test_004(self):
+        assert 1
